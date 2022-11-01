66fd85b477e65f4c44b470ea7df86ad58971d06d
Peter Japhet <peterjaphet111@gmail.com>
2022-11-01 12:01:59 +0200

    created redux slice for auth

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 import App from './App';
-import { AuthContextProvider } from './contexts/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-       <AuthContextProvider>
-     
-        <App />
-      
-    </AuthContextProvider>
+  <React.StrictMode>  
+  <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
